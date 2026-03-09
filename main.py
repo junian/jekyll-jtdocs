dd6a1d21b2a460c8901cee8cbde9e4edebbc6686
Junian Triajianto <author@junian.dev>
2026-03-09 08:14:58 +0700

    feat(main): Generate multiple Pygments CSS themes for syntax highlighting

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from pygments.formatters import HtmlFormatter
 
-# choose any accessible-pygments style
-STYLE = "a11y-dark"
+themes = ["github-light", "github-dark"]
 
-# generate CSS scoped to `.highlight`
-formatter = HtmlFormatter(style=STYLE)
-
-css = formatter.get_style_defs(".highlight")
-
-# write to Jekyll sass directory
-with open("_sass/_pygments.scss", "w") as f:
-    f.write(css)
-
-print("Generated _sass/_pygments.scss")
+for style in themes:
+    formatter = HtmlFormatter(style=style)
+    css = formatter.get_style_defs(".highlight")
+    with open(f"_sass/vendor/accessible-pygments/{style}.scss", "w") as f:
+        f.write(css)
